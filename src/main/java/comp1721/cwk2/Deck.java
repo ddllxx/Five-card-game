2b82c6ab3351909a14ac11cb4d74dbeeb0697b4d
ddllxx <934704716@qq.com>
2022-05-25 22:22:49 +0800

    This version completes the basic implementation of the Deck class and Pokerhand class, and passes the basic test. Signed-off-by: ddllxx <934704716@qq.com>

--- src/main/java/comp1721/cwk2/Deck.java
+++ src/main/java/comp1721/cwk2/Deck.java
@@ -1,3 +1,83 @@
 package comp1721.cwk2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
 // Implement Deck class here
+public class Deck {
+    LinkedList<String> deck = new LinkedList<>();
+
+    public Deck() {
+        ArrayList<String> suit = new ArrayList<>();
+        suit.add("C");
+        suit.add("D");
+        suit.add("H");
+        suit.add("S");
+        String n;
+        for (int s = 0; s <= 3; s++) {
+            for (int i = 1; i < 14; i++) {
+                if (i == 1) {
+                    n = "A";
+                } else if (i == 10) {
+                    n = "T";
+                } else if (i == 11) {
+                    n = "J";
+                } else if (i == 12) {
+                    n = "Q";
+                } else if (i == 13) {
+                    n = "K";
+                } else {
+                    n = i + "";
+                }
+                deck.add(n + suit.get(s));
+            }
+        }
+    }
+
+    public int size() {
+        return deck.size();
+    }
+
+    public boolean isEmpty() {
+        return deck.isEmpty();
+    }
+
+    public boolean contains(Card m) throws CardException {
+        boolean re = false;
+        for (String s : deck) {
+            if (m.toString().equals(s)) {
+                re = true;
+                break;
+            }
+        }
+        return re;
+
+    }
+
+    public void discard() {
+        if (deck.size()==0) {
+            throw new CardException("Wrong!");
+        } else {
+            deck.clear();
+        }
+    }
+
+    public Card deal(){
+        if (deck.size()==0) {
+            throw new CardException("Wrong!");
+        } else {
+            String a = deck.getFirst();
+            deck.pollFirst();
+            return new Card(a);
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    public void add(Card card) throws CardException {
+        deck.add(String.valueOf(card));
+    }
+}
--- src/main/java/comp1721/cwk2/PokerHand.java
+++ src/main/java/comp1721/cwk2/PokerHand.java
@@ -1,3 +1,103 @@
 package comp1721.cwk2;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 // Implement PokerHand class here
+public class PokerHand {
+    LinkedList<String> PokerHand = new LinkedList<>();
+
+    public PokerHand() {
+    }
+
+    public PokerHand(String string) {
+        if (string.length() > 14 || string.length() < 2) {
+            throw new CardException("");
+
+        } else {
+            for (int n = 0; n < (string.length() + 1) / 3; n++) {
+                PokerHand.add(String.valueOf(string.charAt(3 * n)) + string.charAt(3 * n + 1));
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder m = new StringBuilder();
+        for (int i = 0; i < PokerHand.size(); i++) {
+            if (i == PokerHand.size() - 1) {
+                m.append(PokerHand.get(i));
+            } else {
+                m.append(PokerHand.get(i));
+                m.append(" ");
+            }
+        }
+        return m.toString();
+    }
+
+    public int size() {
+        return PokerHand.size();
+    }
+
+    public void discard() {
+        if (PokerHand.size() == 0) {
+            throw new CardException("Wrong!");
+        } else {
+            PokerHand.clear();
+        }
+    }
+
+    public void discardTo(Deck d) {
+        if (PokerHand.size() == 0) {
+            throw new CardException("Wrong!");
+        } else {
+            d.deck.addAll(PokerHand);
+            PokerHand.clear();
+        }
+    }
+
+    public boolean isPair() {
+
+        return false;
+    }
+
+    public boolean isTwoPairs() {
+        return false;
+    }
+
+    public boolean isThreeOfAKind() {
+        return false;
+    }
+
+    public boolean isFourOfAKind() {
+        return false;
+    }
+
+    public boolean isFullHouse() {
+        return false;
+    }
+
+    public boolean isFlush() {
+        return false;
+    }
+
+    public boolean isStraight() {
+        return false;
+    }
+
+    public void add(Card deal) {
+        if (PokerHand.size() == 0) {
+            PokerHand.add(deal.toString());
+
+        } else if (PokerHand.size() == 5) {
+            throw new CardException("wrong!");
+        } else {
+            for (String s : PokerHand) {
+                if (Objects.equals(deal.toString(), s)) {
+                    throw new CardException("wrong!");
+                }
+            }
+            PokerHand.add(deal.toString());
+        }
+    }
+}
